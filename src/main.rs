b0cc6d1638dc4a4356894d2bcc073b8bf5bcda5a
amdRyzen5600X <arca26100@gmail.com>
2024-10-12 01:29:11 +0500

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate glium;
-use glium::Surface;
+use glium::{
+    winit::{event::KeyEvent, keyboard::KeyCode},
+    Surface,
+};
 use hw2::teapot;
 
 fn main() {
@@ -21,47 +24,69 @@ fn main() {
     .unwrap();
 
     let vertex_shader_src = r#"
-            #version 150
+        #version 150
 
-            in vec3 position;
-            in vec3 normal;
+        in vec3 position;
+        in vec3 normal;
 
-            out vec3 v_normal;
+        out vec3 v_normal;
 
-            uniform mat4 matrix;
+        uniform mat4 perspective;
+        uniform mat4 matrix;
 
-            void main() {
-                v_normal = transpose(inverse(mat3(matrix))) * normal;
-                gl_Position = matrix * vec4(position, 1.0);
-            }
-        "#;
+        void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
+            gl_Position = perspective * matrix * vec4(position, 1.0);
+        }
+    "#;
 
     let fragment_shader_src = r#"
-            #version 150
+        #version 150
 
-            in vec3 v_normal;
-            out vec4 color;
-            uniform vec3 u_light;
+        in vec3 v_normal;
+        out vec4 color;
+        uniform vec3 u_light;
 
-            void main() {
-                float brightness = dot(normalize(v_normal), normalize(u_light));
-                vec3 dark_color = vec3(0.6, 0.0, 0.0);
-                vec3 regular_color = vec3(1.0, 0.0, 0.0);
-                color = vec4(mix(dark_color, regular_color, brightness), 1.0);
-            }
-        "#;
+        void main() {
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+        }
+    "#;
 
     let program =
         glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
             .unwrap();
     let mut t: f32 = 0.0;
     let s: f32 = 0.02;
+    let mut z: f32 = 2.0;
+    let mut x: f32 = 0.0;
 
     #[allow(deprecated)]
     event_loop
         .run(move |ev, window_target| {
             match ev {
                 glium::winit::event::Event::WindowEvent { event, .. } => match event {
+                    glium::winit::event::WindowEvent::KeyboardInput {event, .. } => match event {
+                        KeyEvent {physical_key, ..} => {
+                            match physical_key {
+                                glium::winit::keyboard::PhysicalKey::Code(KeyCode::KeyE) => {
+                                    z -= 0.1;
+                                }
+                                glium::winit::keyboard::PhysicalKey::Code(KeyCode::KeyD) => {
+                                    z += 0.1;
+                                }
+                                glium::winit::keyboard::PhysicalKey::Code(KeyCode::KeyS) => {
+                                    x += 0.1;
+                                }
+                                glium::winit::keyboard::PhysicalKey::Code(KeyCode::KeyF) => {
+                                    x -= 0.1;
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
                     glium::winit::event::WindowEvent::CloseRequested => {
                         window_target.exit();
                     }
@@ -80,7 +105,7 @@ fn main() {
                             [0.01 * t.cos(), 0.0, 0.01 * t.sin(), 0.0],
                             [0.0, 0.01, 0.0, 0.0],
                             [-0.01 * t.sin(), 0.0, 0.01 * t.cos(), 0.0],
-                            [0.0, 0.0 + 0.5 * t.sin(), 0.0, 1.0f32],
+                            [x, 0.0 + 0.5 * t.sin(), z, 1.0f32],
                         ];
                         // let matrix = [
                         //     [0.01, 0.0, 0.0, 0.0],
@@ -88,26 +113,39 @@ fn main() {
                         //     [0.0, 0.01*t.sin(), 0.01*t.cos(), 0.0],
                         //     [0.0, 0.0, 0.0, 2.0f32],
                         // ];
+                        let perspective =
+                        {
+                            let (width, height) = target.get_dimensions();
+                            let aspect_ratio = height as f32 / width as f32;
+
+                            let fov: f32 = 3.141592 / 3.0;
+                            let zfar = 1024.0;
+                            let znear = 0.1;
+
+                            let f = 1.0 / (fov / 2.0).tan();
+
+                            [
+                                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+                                [         0.0         ,     f ,              0.0              ,   0.0],
+                                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
+                                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
+                            ]
+                        };
+
                         let light = [-1.0, 0.4, 0.9f32];
 
                         let params = glium::DrawParameters {
                             depth: glium::Depth {
                                 test: glium::draw_parameters::DepthTest::IfLess,
                                 write: true,
-                                ..Default::default()
+                                .. Default::default()
                             },
-                            ..Default::default()
+                            .. Default::default()
                         };
 
-                        target
-                            .draw(
-                                (&positions, &normals),
-                                &indices,
-                                &program,
-                                &uniform! { matrix: matrix, u_light: light },
-                                &params,
-                            )
-                            .unwrap();
+                        target.draw((&positions, &normals), &indices, &program,
+                                    &uniform! { matrix: matrix, perspective: perspective, u_light: light },
+                                    &params).unwrap();
                         target.finish().unwrap();
                     }
                     glium::winit::event::WindowEvent::Resized(window_size) => {
